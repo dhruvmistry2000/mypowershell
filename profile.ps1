f5cab5a8cbea350dbcd19098e17fe9e98580d86b
Dhruv Mistry <68422844+dhruvmistry2000@users.noreply.github.com>
2024-10-19 15:38:47 +0530

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,4 +1,4 @@
- oh-my-posh init pwsh --config 'C:\Users\dhruv\AppData\Local\Programs\oh-my-posh\themes\pure.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config "$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\themes\pure.omp.json" | Invoke-Expression
  Invoke-Expression (& { (zoxide init powershell | Out-String) })
  function winutil {
 	iwr -useb https://christitus.com/win | iex
